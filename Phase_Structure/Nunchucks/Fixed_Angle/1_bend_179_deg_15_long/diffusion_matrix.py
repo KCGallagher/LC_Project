Fixed normalisation of system basis

Phase_Structure/Nunchucks/Fixed_Angle/1_bend_179_deg_15_long/diffusion_matrix.py
@@ -303,7 +303,6 @@ for plot_index, data_index in enumerate(plot_list):
                 rms_disp_values[data_index, i, :, :]
             )
         vec_basis = np.mean(vec_basis_list, axis=0)
-        vec_basis = vec_basis / np.linalg.norm(vec_basis, axis=0)
 
     axis_labels = ["Ax 1", "Ax 2", "Ax 3"]  # until they have been identified
     # axis_labels = ["Director", "Bisector", "Normal"]  # once they have been identified
@@ -315,9 +314,10 @@ for plot_index, data_index in enumerate(plot_list):
 
     if USE_MANUAL_BASIS:
         # SET BASIS MANUALLY
-        vec_basis = np.array([[0, 1, 0], [0.7, 0, 0.7], [0.7, 0, -0.7]])
+        vec_basis = np.array([[0, 1, 0], [0.7, 0, 0.7], [-7, 0, 7]])
         axis_labels = ["Director", "Bisector", "Normal"]
 
+    vec_basis = vec_basis / np.linalg.norm(vec_basis, axis=1)[:, np.newaxis]
     print(vec_basis)
 
     for i in range(len(eq_range)):
@@ -375,6 +375,6 @@ ax.set_ylim((0.01, None))
 axs[int(len(plot_list) / 2)].set_xlabel("Time Step")  # use median of plot_list
 axs[0].set_ylabel(r"RMS Displacement ($\langle x_{i}\rangle^{2}$)")
 fig.legend(loc="center right")
-plt.savefig("rms_displacement_runwise_matrix_cart_rot.png")
+plt.savefig("rms_displacement_runwise_matrix_cart_rot2.png")
 plt.show()
 

Phase_Structure/Nunchucks/Fixed_Angle/Crystalline/No_Transition/diffusion_matrix.py
@@ -298,7 +298,6 @@ for plot_index, data_index in enumerate(plot_list):
                 rms_disp_values[data_index, i, :, :]
             )
         vec_basis = np.mean(vec_basis_list, axis=0)
-        vec_basis = vec_basis / np.linalg.norm(vec_basis, axis=0)
 
     axis_labels = ["Ax 1", "Ax 2", "Ax 3"]  # until they have been identified
     # axis_labels = ["Director", "Bisector", "Normal"]  # once they have been identified
@@ -313,6 +312,7 @@ for plot_index, data_index in enumerate(plot_list):
         vec_basis = np.array([[0, 1, 0], [0.4, 0, 0.9], [0.9, 0, 0.4]])
         axis_labels = ["Director", "Bisector", "Normal"]
 
+    vec_basis = vec_basis / np.linalg.norm(vec_basis, axis=1)[:, np.newaxis]
     print(vec_basis)
 
     for i in range(len(eq_range)):